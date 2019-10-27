6d91bddbf0385abe70539991bbcb59b5bf512eed
Pete Misik <mirtec001@hotmail.com>
2019-10-27 10:43:41 -0400

    Updated files to work with the computer
    Minor changes to Window1.xaml and Window1.cs

--- Window1.cs
+++ Window1.cs
@@ -19,11 +19,13 @@ namespace CVPApp
 
         public Window1()
         {
+           // MessageBox.Show("InitializeComponent", "IC", MessageBoxButton.OK);
            InitializeComponent();
         }
 
         private void OpenClick(object sender, EventArgs e)
         {
+	        // MessageBox.Show("openclick", "OC", MessageBoxButton.OK);
             OpenFileDialog ofd = new OpenFileDialog();
             Nullable<bool> result = ofd.ShowDialog();
             
@@ -41,26 +43,36 @@ namespace CVPApp
 
         private void PlayClick(object sender, EventArgs e)
         {
-            if (lb.SelectedItem != null)
+            MessageBox.Show("playclick", "PC", MessageBoxButton.OK);
+            try{
+
+                    if (lb.SelectedItem != null)
+                    {
+                        win2 = new Window2(lb.SelectedItem.ToString());
+                        win2.WindowStartupLocation = WindowStartupLocation.Manual;
+                        win2.Left = 1610;
+                        win2.Top = 0;
+                        win2.Width = 1600;
+                        win2.Height = 900;
+                        win2.WindowStyle = WindowStyle.None;
+                        win2.Show();
+                        win2.PlayMedia();
+                    }
+                }
+            catch(Exception ex) 
             {
-                win2 = new Window2(lb.SelectedItem.ToString());
-                win2.WindowStartupLocation = WindowStartupLocation.Manual;
-                win2.Left = 1912;
-                win2.Top = 0;
-                win2.Width = 1945;
-                win2.Height = 1225;
-                win2.WindowStyle = WindowStyle.None;
-                win2.Show();
-                win2.PlayMedia();
-            }
+		        MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK);
+	        }
         }
         private void StopClick(object sender, EventArgs e)
         {
+	        // MessageBox.Show("stopclick", "SC", MessageBoxButton.OK);
             win2.StopMedia();
         }
         
         private void CloseClick(object sender, EventArgs e)
         {
+	        // MessageBox.Show("closeclick", "CC", MessageBoxButton.OK);
             win2.Close();
             // this.Close();
         }
